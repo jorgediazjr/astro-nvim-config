a098d02be2b8207e4424200211ea6ecddadec8b2
jorgediazjr <jorge7soccer@gmail.com>
2023-04-17 17:24:11 -0400

    Use kanagawa colorscheme

--- plugins/core.lua
+++ plugins/core.lua
@@ -29,6 +29,15 @@ return {
       vim.cmd.colorscheme "catppuccin"
     end,
   },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd.colorscheme "kanagawa"
+    end,
+  },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
